80d41451a0c6fb40906b1c8aaaaeb8ce64912af4
gilbert-khonstantine <khonstantine@gmail.com>
2020-06-03 11:47:44 +0800

    seperating reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,17 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Leaders } from './leaders';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Dishes } from './dishes';
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, initialState
+        combineReducers({
+            leaders: Leaders,
+            dishes: Dishes,
+            promotions: Promotions,
+            comments: Comments,
+        })
     )
     return store;
 }
